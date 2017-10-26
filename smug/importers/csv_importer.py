2002227d62f8c422c221fd1285a71edecf1bdc66
Rick Rongen <rickrongen@outlook.com>
2017-10-26 11:31:08 +0200

    feat: separated importer storing and reading

--- smug/importers/csv_importer.py
+++ smug/importers/csv_importer.py
@@ -1,15 +1,13 @@
 from argparse import ArgumentParser
 import pandas as pd
 import pkg_resources
-import hashlib
 
 from smug.connection_manager import ConnectionManager
 
+from .pandas_importer import PandasImporter
 
-class CsvImporter:
-    def __init__(self, connection_mananger):
-        self.connection_manager = connection_mananger
 
+class CsvImporter(PandasImporter):
     def process_files(self, files):
         for file in files:
             self.process_file(file)
@@ -20,18 +18,7 @@ class CsvImporter:
         df = pd.read_csv(filename, sep=';')
         df = df.drop_duplicates(subset='url', keep='first')
 
-        dtypes = str(df.dtypes.values)
-        dtypes_hash = hashlib.md5(dtypes.encode()).hexdigest()
-
-        type = ''
-        if dtypes_hash == '72197921735611c48d8114efb03740ce':
-            type += 'formatter.coosto'
-
-        for inex, row in df.iterrows():
-            if type != '':
-                connection_manager.publish_to_exchange(type, row.to_json())
-            else:
-                connection_manager.publish_to_queue('cleaning', row.to_json())
+        self.process_dataframe(df)
 
 
 if __name__ == '__main__':
