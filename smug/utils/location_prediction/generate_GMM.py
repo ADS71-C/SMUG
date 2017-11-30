8507960769a7eed395a55e3138fa55e09e633dfd
Mike Evers <mike_evers1997@hotmail.com>
2017-11-30 14:28:51 +0100

    add english stopwords and refactor

---
+++ smug/utils/location_prediction/generate_GMM.py
@@ -0,0 +1,43 @@
+# todo: Dockerize spark so findspark can be deleted.
+import findspark
+findspark.init()
+findspark.find()
+
+import os
+import pkg_resources
+from pyspark.sql import SparkSession
+
+if __name__ == '__main__':
+    spark = SparkSession \
+        .builder \
+        .appName("Proftaak") \
+        .getOrCreate()
+
+    sc = spark.sparkContext
+
+    # Share the soft-boiled library with the cluster
+    soft_boiled = pkg_resources.resource_filename('resources', 'soft-boiled.zip')
+    sc.addPyFile(soft_boiled)
+    from src.algorithms import gmm
+
+    # Load the train data in chunks and filter tweets with a location.
+    tweets_df = None
+    for filename in os.listdir('train_dataset_small'):
+        print(filename)
+        chunk = spark.read.json('train_dataset_small/' + filename)
+        chunk = chunk.filter('geo is not null')
+        chunk = chunk.filter('place is not null')
+        chunk = chunk.select(['user', 'text', 'geo', 'place', 'entities.urls', 'extended_entities.media'])
+        if tweets_df is not None:
+            tweets_df = tweets_df.union(chunk)
+        else:
+            tweets_df = chunk
+
+    tweets_df.createOrReplaceTempView('my_tweets')
+
+    # Train and create the model
+    gmm_model = gmm.train_gmm(spark, 'my_tweets', ['user.location', 'text'], min_occurrences=5, max_num_components=12)
+
+    # Save model for future prediction use
+    path = pkg_resources.resource_filename('resources', 'location_prediction.csv')
+    gmm.save_model(gmm_model, path)
